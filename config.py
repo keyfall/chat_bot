39da6108154f9753d6f7f4cdd9c266a773d2dc4a
keyfall <2536726426@qq.com>
2024-06-06 18:44:19 +0800

    stop_words

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 
 user_dict_path = "corpus/user_dict/user_dict.txt"
+stop_words_path = "corpus/stop_words/cn_stopwords.txt"
---
+++ prepar_corpus/prepar_user_dict/test_stop_words.py
@@ -0,0 +1,8 @@
+
+import jieba
+import config
+
+
+def test_stop_words(words):
+    filter_words = [word for word in words if word not in ]
+    return
